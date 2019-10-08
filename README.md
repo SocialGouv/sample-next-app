1c7b61a1a787843d80dd18f5fac616e275e2a505
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-10-08 17:20:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# sample next.js app
+
+A sample Next.js app with :
+
+- sentry
+- matomo
+- docker
+- gitlab-ci
+- @SocialGouv linters
+- @SocialGouv bootstrap
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Page = () => (
+  <div>
+    <h3>Hello, world</h3>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>
+      <button>trigger Matomo event</button>
+    </p>
+    <p>
+      <button>trigger Sentry event</button>
+    </p>
+  </div>
+);
+
+export default Page;
