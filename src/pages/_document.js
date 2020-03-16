90e1323beac3605df5662a3523ac88fa120b132e
Douglas Duteil <douglasduteil@gmail.com>
2020-03-16 16:58:10 +0100

    feat: import bootstrap from npm (#144)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,11 +14,6 @@ class MyDocument extends Document {
           {/* see polyfill.io */}
           <script src="//polyfill.incubateur.social.gouv.fr/v3/polyfill.min.js"></script>
           <link rel="shortcut icon" href="/favicon.ico" />
-          <link
-            rel="stylesheet"
-            href="//socialgouv.github.io/bootstrap/master/@socialgouv/bootstrap.core/dist/socialgouv-bootstrap.min.css"
-            integrity="grB93+Lj8+H7BK2kCBM0dGAJD+8tE7pYqy6qkcykr8DfCJt7PhJOF998Bwj7BAWc"
-          />
         </Head>
         <body>
           <Main />
