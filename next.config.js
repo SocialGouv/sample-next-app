23d5b872f8424fe7b2d5d7b8385fda2666e5632a
LionelB <lionel@lumographe.fr>
2020-05-13 14:32:56 +0200

    fix: review

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ module.exports = nextSourceMaps({
     MATOMO_URL: process.env.MATOMO_URL,
     REFRESH_TOKEN_EXPIRES: process.env.REFRESH_TOKEN_EXPIRES || 43200,
     JWT_TOKEN_EXPIRES: process.env.JWT_TOKEN_EXPIRES || 15,
+    ACTIVATION_TOKEN_EXPIRES: process.env.ACTIVATION_TOKEN_EXPIRES || 10080,
   },
   webpack: (config, { isServer, buildId }) => {
     config.plugins.push(
