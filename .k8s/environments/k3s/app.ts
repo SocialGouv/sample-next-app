4878a7c71c5e4d2a96272299adbba76d43c01105
Douglas Duteil <douglasduteil@gmail.com>
2020-05-19 16:02:38 +0200

    build(k8s): use gitlab env as base

--- .k8s/environments/k3s/app.ts
+++
@@ -1,6 +0,0 @@
-import { AppComponentEnvironment } from "../../types";
-
-export default {
-  imageTag: "latest",
-  imageName: "localhost:5000/socialgouv/sample-next-app",
-} as Readonly<AppComponentEnvironment>;
