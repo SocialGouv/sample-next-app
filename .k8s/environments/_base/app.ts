7517061c3a8b37dcda344ea14c556289e9fb678b
Douglas Duteil <douglasduteil@gmail.com>
2020-05-19 16:29:37 +0200

    build(k8s): use gitlab env in app

--- .k8s/environments/_base/app.ts
+++ .k8s/environments/_base/app.ts
@@ -2,8 +2,11 @@ import { AppComponentEnvironment } from "@socialgouv/kosko-charts/types";
 export default {
   name: "www",
   image: {
-    name: "registry.gitlab.factory.social.gouv.fr/socialgouv/sample-next-app",
-    tag: "1118b7914282ed114c77a1338d83330e90185777",
+    name: process.env.CI_REGISTRY_IMAGE,
+    tag: process.env.CI_COMMIT_SHA,
+  },
+  labels: {
+    component: "next",
   },
   containerPort: 8080,
   servicePort: 8080,
