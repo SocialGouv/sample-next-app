93f22a3931e7969bd2c015179a96771558bd0a2d
Douglas Duteil <douglasduteil@gmail.com>
2020-05-22 16:02:11 +0200

    feat(k8s): add beta prefix in production

--- .k8s/environments/prod/app.ts
+++ .k8s/environments/prod/app.ts
@@ -1,5 +1,5 @@
 import { GlobalEnvironment } from "@socialgouv/kosko-charts/types";
 
 export default {
-  subdomain: `alpha-${process.env.CI_PROJECT_NAME as string}`,
+  subdomain: `beta-${process.env.CI_PROJECT_NAME as string}`,
 } as Partial<GlobalEnvironment>;
