787c966b6f2d7141315ac658bd38985c21d30fa9
Douglas Duteil <douglasduteil@gmail.com>
2020-05-26 18:21:32 +0200

    build(k8s): and configmap as first lvl env

--- .k8s/components/app.ts
+++ .k8s/components/app.ts
@@ -1,7 +1,29 @@
 import { create } from "@socialgouv/kosko-charts/components/app";
+import { metadataFromParams } from "@socialgouv/kosko-charts/components/app/metadata";
 import env from "@kosko/env";
+import { ConfigMap } from "kubernetes-models/v1/ConfigMap";
 
 const params = env.component("app");
 const { deployment, ingress, service } = create(params);
 
-export default [deployment, ingress, service];
+//
+
+const envConfigMap = new ConfigMap({
+  metadata: {
+    ...metadataFromParams(params),
+    name: `${params.name}-env`
+  },
+  data: {
+    NODE_ENV: process.env.NODE_ENV || "production"
+  }
+})
+
+deployment.spec!.template.spec!.containers[0].envFrom = [
+  {
+    configMapRef: {name: `${params.name}-env`}
+  }
+];
+
+//
+
+export default [deployment, ingress, service, envConfigMap];
