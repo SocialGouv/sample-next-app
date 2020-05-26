07790a289219d207566c00a8cb210dc6032478c1
Douglas Duteil <douglasduteil@gmail.com>
2020-05-26 18:12:50 +0200

    Revert "fix(k8s): use nginx ingress class"
    This reverts commit caff4b571095f9da4c5c6bf37f27aa804750f323.

--- .k8s/environments/_base/index.ts
+++ .k8s/environments/_base/index.ts
@@ -1,14 +1,3 @@
 import gitlab from "@socialgouv/kosko-charts/environments/gitlab";
-import { merge } from "@socialgouv/kosko-charts/utils/merge";
-import { GlobalEnvironment } from "@socialgouv/kosko-charts/types";
 
-export default merge(gitlab(process.env), {
-  ingress: {
-    annotations: {
-      "appgw.ingress.kubernetes.io/ssl-redirect": undefined,
-      "certmanager.k8s.io/cluster-issuer": "letsencrypt-prod",
-      "kubernetes.io/ingress.class": "nginx",
-      "kubernetes.io/tls-acme": "true",
-    }
-  }
-} as Partial<GlobalEnvironment>);
+export default gitlab(process.env);
