caff4b571095f9da4c5c6bf37f27aa804750f323
Douglas Duteil <douglasduteil@gmail.com>
2020-05-26 15:24:17 +0200

    fix(k8s): use nginx ingress class

--- .k8s/environments/_base/index.ts
+++ .k8s/environments/_base/index.ts
@@ -1,3 +1,14 @@
 import gitlab from "@socialgouv/kosko-charts/environments/gitlab";
+import { merge } from "@socialgouv/kosko-charts/utils/merge";
+import { GlobalEnvironment } from "@socialgouv/kosko-charts/types";
 
-export default gitlab(process.env);
+export default merge(gitlab(process.env), {
+  ingress: {
+    annotations: {
+      "appgw.ingress.kubernetes.io/ssl-redirect": undefined,
+      "certmanager.k8s.io/cluster-issuer": "letsencrypt-prod",
+      "kubernetes.io/ingress.class": "nginx",
+      "kubernetes.io/tls-acme": "true",
+    }
+  }
+} as Partial<GlobalEnvironment>);
