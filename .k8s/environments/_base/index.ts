8f17590a338f179b370ad790d5740fdc71d7aa4b
Douglas Duteil <douglasduteil@gmail.com>
2020-05-28 18:49:05 +0200

    build(k8s): multi cluster

--- .k8s/environments/_base/index.ts
+++ .k8s/environments/_base/index.ts
@@ -1,3 +1,25 @@
 import gitlab from "@socialgouv/kosko-charts/environments/gitlab";
+import { ok } from "assert";
 
-export default gitlab(process.env);
+const gitlabEnv =
+  process.env.AUTO_DEVOPS_DEV_ENVIRONMENT_NAME ||
+  process.env.AUTO_DEVOPS_PREPROD_ENVIRONMENT_NAME ||
+  process.env.AUTO_DEVOPS_PROD_ENVIRONMENT_NAME;
+
+ok(gitlabEnv);
+
+const ingress = gitlabEnv.endsWith("-dev")
+  ? {
+      ingress: {
+        annotations: {
+          "appgw.ingress.kubernetes.io/ssl-redirect": "true",
+          "kubernetes.io/ingress.class": "azure/application-gateway",
+        },
+        secretName: "wildcard-crt",
+      },
+    }
+  : {};
+export default {
+  ...gitlab(process.env),
+  ...ingress,
+};
