8235144ddb2a8007cea67c4fad829a285861263d
Douglas Duteil <douglasduteil@gmail.com>
2020-05-28 12:32:41 +0200

    build(k8s): support k8s v1.14 too

--- .k8s/components/app.ts
+++ .k8s/components/app.ts
@@ -1,3 +1,4 @@
+import { ok } from "assert";
 import { create } from "@socialgouv/kosko-charts/components/app";
 import { metadataFromParams } from "@socialgouv/kosko-charts/components/app/metadata";
 import env from "@kosko/env";
@@ -7,21 +8,32 @@ const params = env.component("app");
 const { deployment, ingress, service } = create(params);
 
 //
+ok(process.env.CI_ENVIRONMENT_NAME);
+if (
+  process.env.CI_ENVIRONMENT_NAME.endsWith("-dev") ||
+  process.env.CI_ENVIRONMENT_NAME.endsWith("prod")
+) {
+  // HACK(douglasduteil): our cluster v1 is not supporting the `startupProbe`
+  // Our cluster v1 is stuck in k8s v1.14 :(
+  delete deployment.spec!.template.spec!.containers[0].startupProbe;
+}
+
+//
 
 const envConfigMap = new ConfigMap({
   metadata: {
     ...metadataFromParams(params),
-    name: `${params.name}-env`
+    name: `${params.name}-env`,
   },
   data: {
-    NODE_ENV: process.env.NODE_ENV || "production"
-  }
-})
+    NODE_ENV: process.env.NODE_ENV || "production",
+  },
+});
 
 deployment.spec!.template.spec!.containers[0].envFrom = [
   {
-    configMapRef: {name: `${params.name}-env`}
-  }
+    configMapRef: { name: `${params.name}-env` },
+  },
 ];
 
 //
