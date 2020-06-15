2f7b173be1c0c7e4700036d3611867d9add207b1
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-06-15 10:53:14 +0200

    feat(secu): add CSP

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,12 @@
 import React from "react";
+import crypto from "crypto";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+
+const cspHashOf = (text) => {
+  const hash = crypto.createHash("sha256");
+  hash.update(text);
+  return `'sha256-${hash.digest("base64")}'`;
+};
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -8,9 +15,23 @@ class MyDocument extends Document {
   }
 
   render() {
+    const scriptSrc = [
+      "'self'",
+      cspHashOf(NextScript.getInlineScriptSource(this.props)),
+    ];
+    if (process.env.NODE_ENV !== "production") {
+      // for next.js dev server
+      scriptSrc.push("'unsafe-eval'");
+      scriptSrc.push("'unsafe-inline'");
+    }
+    const csp = `default-src 'self'; script-src ${scriptSrc.join(
+      " "
+    )}; style-src 'unsafe-inline'`;
+
     return (
-      <Html>
+      <Html lang="fr">
         <Head>
+          <meta httpEquiv="Content-Security-Policy" content={csp} />
           <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
         <body>
