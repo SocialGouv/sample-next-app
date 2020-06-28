dde19153a8dfa85a68a045cecedc8ea56d36240b
Douglas Duteil <douglasduteil@gmail.com>
2020-06-28 13:29:11 +0200

    docs(k8s): add comment for the AUTO_DEVOPS_*_ENVIRONMENT_NAME hack

--- .k8s/environments/_base/index.ts
+++ .k8s/environments/_base/index.ts
@@ -6,7 +6,12 @@ const gitlabEnv =
   process.env.AUTO_DEVOPS_PREPROD_ENVIRONMENT_NAME ||
   process.env.AUTO_DEVOPS_PROD_ENVIRONMENT_NAME;
 
-ok(gitlabEnv);
+ok(
+  gitlabEnv,
+  "Expect an AUTO_DEVOPS_*_ENVIRONMENT_NAME to be defined\n" +
+    "HACK(douglasduteil): ensure that the sample-next-app is still deployable to our cluster v1\n" +
+    "We should remove this section in the future ;)"
+);
 
 const ingress = gitlabEnv.endsWith("-dev")
   ? {
