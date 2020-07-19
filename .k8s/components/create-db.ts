360601da9a8946aa162bad3727a4dc97a15ca469
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-07-20 00:26:18 +0200

    generate yaml with create_db

--- .k8s/components/create-db.ts
+++ .k8s/components/create-db.ts
@@ -9,9 +9,23 @@ ok(process.env.CI_COMMIT_SHORT_SHA, "Expect CI_COMMIT_SHORT_SHA to be defined");
 
 const defaultExport = [];
 
-const { createDbJob: job } = createDb(env.component("create-db"));
+console.log("CI_COMMIT_SHORT_SHA", process.env.CI_COMMIT_SHORT_SHA);
+console.log("env.component('create-db')", env.component("create-db"));
+let params = env.component("create-db");
+params.database = "yopyop";
+params.password = "yopyop";
+params.user = "yopyop";
+
+const { createDbJob: job } = createDb(params);
 
 // creates azure-pg-user secret
-const { createSecret: secret } = createSecret(env.component("pg-secret"));
+params = env.component("pg-secret");
+params.database = "yopyop";
+params.password = "yopyop";
+params.user = "yopyop";
+params.host = "yopyop";
+
+// const { createSecret: secret } = createSecret(env.component("pg-secret"));
+const { createSecret: secret } = createSecret(params);
 
 export default [job, secret];
