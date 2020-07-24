ab1e0e9754cb8e6233c82cac65235a859c368cd8
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-07-24 07:23:02 +0200

    wip

--- .k8s/components/app.ts
+++ .k8s/components/app.ts
@@ -1,71 +1,11 @@
 import { ok } from "assert";
 import { create } from "@socialgouv/kosko-charts/components/app";
-import { metadataFromParams } from "@socialgouv/kosko-charts/components/app/metadata";
-import env from "@kosko/env";
-import { addToEnvFrom } from "@socialgouv/kosko-charts/utils/addToEnvFrom";
-import { ConfigMap } from "kubernetes-models/v1/ConfigMap";
-import { EnvFromSource } from "kubernetes-models/v1/EnvFromSource";
-import { SealedSecret } from "@kubernetes-models/sealed-secrets/bitnami.com/v1alpha1/SealedSecret";
-import { loadYaml } from "../getEnvironmentComponent";
 
 ok(process.env.CI_COMMIT_SHORT_SHA, "Expect CI_COMMIT_SHORT_SHA to be defined");
 
-const params = env.component("app");
-const { deployment, ingress, service } = create(params);
+//const params = env.component("app");
+const manifests = create();
 
-const secret = new SealedSecret({
-  ...loadYaml(env, "app-env.sealed-secret.yaml"),
-  metadata: {
-    ...metadataFromParams(params),
-    name: `app-env`,
-    annotations: {
-      "sealedsecrets.bitnami.com/cluster-wide": "true",
-    },
-  },
-});
+console.log("manifests", manifests);
 
-const configMap = new ConfigMap({
-  ...loadYaml(env, "app-env.configmap.yaml"),
-  metadata: {
-    ...metadataFromParams(params),
-    name: `app-env`,
-  },
-  data: {
-    FRONTEND_HOST: ingress.spec!.rules![0].host!,
-  },
-});
-
-//
-
-const secretSource = new EnvFromSource({
-  secretRef: {
-    name: `app-env`,
-  },
-});
-
-const configMapSource = new EnvFromSource({
-  configMapRef: {
-    name: `app-env`,
-  },
-});
-
-addToEnvFrom({
-  deployment,
-  data: [secretSource, configMapSource],
-});
-
-if (process.env.ENABLE_AZURE_POSTGRES) {
-  const azureSecretSource = new EnvFromSource({
-    secretRef: {
-      name: `azure-pg-user-${process.env.CI_COMMIT_SHORT_SHA}`,
-    },
-  });
-  addToEnvFrom({
-    deployment,
-    data: [azureSecretSource],
-  });
-}
-
-//
-
-export default [secret, configMap, deployment, ingress, service];
+export default manifests;
