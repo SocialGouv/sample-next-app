4e84b9125aca072d0cbbd3ffdeb41651760949c4
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-07-25 01:56:02 +0200

    progress

--- .k8s/environments/prod/app.ts
+++ .k8s/environments/prod/app.ts
@@ -1,5 +1,3 @@
-import { GlobalEnvironment } from "@socialgouv/kosko-charts/types";
-
 export default {
   subdomain: `beta-${process.env.CI_PROJECT_NAME as string}`,
-} as Partial<GlobalEnvironment>;
+};
