e26560d4f5ae4bcfd2acccdc1d36707313673250
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-07-24 13:28:27 +0200

    wip

--- .k8s/components/app.ts
+++ .k8s/components/app.ts
@@ -1,10 +1,11 @@
 import { ok } from "assert";
 import { create } from "@socialgouv/kosko-charts/components/app";
 
+import env from "@kosko/env";
 ok(process.env.CI_COMMIT_SHORT_SHA, "Expect CI_COMMIT_SHORT_SHA to be defined");
 
 //const params = env.component("app");
-const manifests = create();
+const manifests = create(env);
 
 console.log("manifests", manifests);
 
