a0b9089d7c2effec607de4f4a7f9512316691fad
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-07-26 00:39:50 +0200

    add pg user to pgweb+hasura

--- .k8s/components/hasura.ts
+++ .k8s/components/hasura.ts
@@ -1,10 +1,13 @@
 import env from "@kosko/env";
 
 import { create } from "@socialgouv/kosko-charts/components/app";
+import { addToEnvFrom } from "@socialgouv/kosko-charts/utils/addToEnvFrom";
+import { EnvFromSource } from "kubernetes-models/v1/EnvFromSource";
 
 const manifests = create("hasura", {
   env,
   config: {
+    //ingress: false,
     image:
       "registry.gitlab.factory.social.gouv.fr/socialgouv/sample-next-app/hasura:002b0f68266e5243e733fe7c896b610f49757718",
     subdomain: `hasura-${process.env.CI_PROJECT_NAME as string}`,
@@ -20,17 +23,21 @@ const manifests = create("hasura", {
   },
 });
 
-// if (process.env.ENABLE_AZURE_POSTGRES) {
-//   const azureSecretSource = new EnvFromSource({
-//     secretRef: {
-//       name: `azure-pg-user-${process.env.CI_COMMIT_SHORT_SHA}`,
-//     },
-//   });
-//   addToEnvFrom({
-//     deployment,
-//     data: [azureSecretSource],
-//   });
-// }
+// DEV: add secret to access DB
+//@ts-expect-error
+const deployment = manifests.find((manifest) => manifest.kind === "Deployment");
+if (deployment) {
+  const azureSecretSource = new EnvFromSource({
+    secretRef: {
+      name: `azure-pg-user-${process.env.CI_COMMIT_SHORT_SHA}`,
+    },
+  });
+  addToEnvFrom({
+    //@ts-expect-error
+    deployment,
+    data: [azureSecretSource],
+  });
+}
 
 //
 
--- .k8s/components/pgweb.ts
+++ .k8s/components/pgweb.ts
@@ -1,6 +1,8 @@
 import env from "@kosko/env";
 
 import { create } from "@socialgouv/kosko-charts/components/app";
+import { addToEnvFrom } from "@socialgouv/kosko-charts/utils/addToEnvFrom";
+import { EnvFromSource } from "kubernetes-models/v1/EnvFromSource";
 
 const manifests = create("pgweb", {
   env,
@@ -26,6 +28,22 @@ const manifests = create("pgweb", {
     containerPort: 8081,
   },
 });
+
+// DEV: add secret to access DB
+//@ts-expect-error
+const deployment = manifests.find((manifest) => manifest.kind === "Deployment");
+if (deployment) {
+  const azureSecretSource = new EnvFromSource({
+    secretRef: {
+      name: `azure-pg-user-${process.env.CI_COMMIT_SHORT_SHA}`,
+    },
+  });
+  addToEnvFrom({
+    //@ts-expect-error
+    deployment,
+    data: [azureSecretSource],
+  });
+}
 
 export default manifests;
 
