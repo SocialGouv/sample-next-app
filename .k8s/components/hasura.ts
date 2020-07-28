56c76f1efae2742aecbea334695d43b9843ae3be
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-07-28 02:05:58 +0200

    fix: up @socialgouv/kosko-charts:v2.0.0-alpha.15

--- .k8s/components/hasura.ts
+++ .k8s/components/hasura.ts
@@ -1,47 +1,10 @@
 import env from "@kosko/env";
 
-import { create } from "@socialgouv/kosko-charts/components/app";
-import { addPostgresUserSecret } from "@socialgouv/kosko-charts/utils/addPostgresUserSecret";
+import { create } from "@socialgouv/kosko-charts/components/hasura";
 
 // todo: extract to @socialgouv/kosko-charts/components/hasura
 const manifests = create("hasura", {
   env,
-  config: {
-    //ingress: false,
-    subdomain: `hasura-${process.env.CI_PROJECT_NAME as string}`,
-    requests: {
-      cpu: "100m",
-      memory: "64Mi",
-    },
-    limits: {
-      cpu: "500m",
-      memory: "256Mi",
-    },
-    livenessProbe: {
-      httpGet: {
-        path: "/healthz",
-        port: "http",
-      },
-      initialDelaySeconds: 60,
-      periodSeconds: 20,
-    },
-    readinessProbe: {
-      httpGet: {
-        path: "/healthz",
-        port: "http",
-      },
-      initialDelaySeconds: 60,
-      periodSeconds: 20,
-    },
-    containerPort: 80,
-  },
 });
-
-// DEV: add secret to access DB
-//@ts-expect-error
-const deployment = manifests.find((manifest) => manifest.kind === "Deployment");
-addPostgresUserSecret(deployment);
-
-//
 
 export default manifests;
