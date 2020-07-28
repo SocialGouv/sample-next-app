98976d4b4bb61aaf6a7a53fe51155fafcc770a45
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-07-28 02:28:48 +0200

    fix: use components/hasura

--- .k8s/components/hasura.ts
+++ .k8s/components/hasura.ts
@@ -3,8 +3,9 @@ import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/hasura";
 
 // todo: extract to @socialgouv/kosko-charts/components/hasura
-const manifests = create("hasura", {
+const manifests = create({
   env,
+  config: {},
 });
 
 export default manifests;
