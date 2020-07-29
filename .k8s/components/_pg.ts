a971f4dc525b34cc3e13a0a8d6e11e77b7318a3f
Douglas Duteil <douglasduteil@gmail.com>
2020-07-29 17:03:39 +0200

    build(k8s): no create db in preprod

--- .k8s/components/_pg.ts
+++ .k8s/components/_pg.ts
@@ -1,12 +1,9 @@
 import env from "@kosko/env";
-import { ok } from "assert";
 
 import { create } from "@socialgouv/kosko-charts/components/azure-pg";
-
-let params = env.component("pg");
 
 const manifests = create({
   env,
 });
 
-export default manifests;
+export default env.env === "dev" ? manifests : [];
