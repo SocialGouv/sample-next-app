9f922d16d49e7f4007491357077265dbfe765f3a
Douglas Duteil <douglasduteil@gmail.com>
2020-07-29 14:15:07 +0200

    fix(k8s): update @socialgouv/kosko-charts to 2.0.0-alpha.26
    `yarn k8s upgrade @socialgouv/kosko-charts@alpha`

--- .k8s/components/hasura.ts
+++ .k8s/components/hasura.ts
@@ -2,10 +2,8 @@ import env from "@kosko/env";
 
 import { create } from "@socialgouv/kosko-charts/components/hasura";
 
-// todo: extract to @socialgouv/kosko-charts/components/hasura
 const manifests = create({
   env,
-  config: {},
 });
 
 export default manifests;
