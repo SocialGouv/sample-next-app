8054aa2bee6d8806abb730f760f866d8e2e04c3e
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-07-29 14:13:28 +0200

    rename pg secrets

--- .k8s/components/_pg.ts
+++ .k8s/components/_pg.ts
@@ -3,11 +3,10 @@ import { ok } from "assert";
 
 import { create } from "@socialgouv/kosko-charts/components/azure-pg";
 
-let params = env.component("azure-pg");
+let params = env.component("pg");
 
 const manifests = create({
   env,
-  config: {},
 });
 
 export default manifests;
