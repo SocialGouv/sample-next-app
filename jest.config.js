6818756bf314efd854ea508ff16ddb0d523f8436
Douglas Duteil <douglasduteil@gmail.com>
2020-12-10 14:48:40 +0100

    chore: make the code run on node@15 (#430)

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/.k8s/", "/node_modules/"],
+};
