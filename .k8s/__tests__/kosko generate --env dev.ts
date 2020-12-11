49f49ee2ed032301e244a2cf5453b795bbde5cdc
Douglas DUTEIL <douglasduteil@gmail.com>
2020-12-11 16:04:06 +0100

    build(k8s): update testing system*

--- .k8s/__tests__/kosko generate --env dev.ts
+++ .k8s/__tests__/kosko generate --env dev.ts
@@ -6,6 +6,10 @@ import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 jest.setTimeout(1000 * 60);
 test("kosko generate --dev", async () => {
   expect(
-    await getEnvManifests("dev", "", project("sample-next-app").dev)
+    await getEnvManifests("dev", "", {
+      ...project("sample-next-app").dev,
+      KUBE_NAMESPACE: "sample-next-app-85-master-dev2",
+      RANCHER_PROJECT_ID: "c-bd7z2:p-7ms8p",
+    })
   ).toMatchSnapshot();
 });
--- .k8s/__tests__/kosko generate --env preprod.ts
+++ .k8s/__tests__/kosko generate --env preprod.ts
@@ -6,6 +6,10 @@ import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 jest.setTimeout(1000 * 60);
 test("kosko generate --preprod", async () => {
   expect(
-    await getEnvManifests("preprod", "", project("sample-next-app").preprod)
+    await getEnvManifests("preprod", "", {
+      ...project("sample-next-app").preprod,
+      KUBE_NAMESPACE: "sample-next-app-85-preprod-dev2",
+      RANCHER_PROJECT_ID: "c-bd7z2:p-7ms8p",
+    })
   ).toMatchSnapshot();
 });
