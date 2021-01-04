cc7a1afa6b126cbbc276b4497ff9fbfa08178170
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-01-04 18:18:31 +0100

    fix: test kapp annotation

--- .k8s/components/_pgweb.ts
+++ .k8s/components/_pgweb.ts
@@ -1,6 +1,14 @@
 import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/pgweb";
 
-const manifests = create("pgweb", { env });
+const manifests = create("pgweb", {
+  env,
+  config: {
+    annotations: {
+      "kapp.k14s.io/disable-default-ownership-label-rules": "",
+      "kapp.k14s.io/disable-default-label-scoping-rules": "",
+    },
+  },
+});
 
 export default manifests;
--- .k8s/components/app.ts
+++ .k8s/components/app.ts
@@ -4,7 +4,13 @@ import { create } from "@socialgouv/kosko-charts/components/app";
 
 const manifests = create("app", {
   env,
-  config: { containerPort: 3030 },
+  config: {
+    containerPort: 3030,
+    annotations: {
+      "kapp.k14s.io/disable-default-ownership-label-rules": "",
+      "kapp.k14s.io/disable-default-label-scoping-rules": "",
+    },
+  },
   deployment: {
     container: {
       resources: {
