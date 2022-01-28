6e693da977c9cf22f8d0301ee63ab1ea533e62f8
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-01-29 00:07:03 +0100

    try without az prefix

--- .socialgouv/components/yaml.ts
+++ .socialgouv/components/yaml.ts
@@ -1,8 +1,26 @@
 import env from "@kosko/env";
+import { loadFile } from "@kosko/yaml";
 import { importYamlFolder } from "@socialgouv/kosko-charts/components/yaml";
 import path from "path";
+import { existsSync } from "fs";
 
-const manifests = importYamlFolder(
-  path.join(__dirname, "..", `environments/${env.env}/yaml`)
-);
-export default manifests;
+const getManifests = async () => {
+  const manifests = [];
+
+  // files in env/[env]/yaml/*
+  manifests.push(
+    importYamlFolder(path.join(__dirname, "..", `environments/${env.env}/yaml`))
+  );
+
+  // add pg-user if any env/[env]/pg-user.sealed-secret.yaml
+  const pgUserPath = path.join(
+    __dirname,
+    `../environments/${env.env}/pg-user.sealed-secret.yaml`
+  );
+  if (existsSync(pgUserPath)) {
+    manifests.push(loadFile(pgUserPath));
+  }
+  return manifests;
+};
+
+export default getManifests;
